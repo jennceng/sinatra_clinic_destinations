867ef6d5c582c85f7629b2d14d4365ee4c88664f
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-09-06 15:25:16 -0400

    live code

--- server.rb
+++ server.rb
@@ -1,2 +1,22 @@
 require 'sinatra'
 require 'pry'
+
+get "/" do
+  redirect "/destinations"
+end
+
+get "/destinations" do
+  @destinations = File.readlines('destinations.txt')
+
+  erb :index
+end
+
+post "/destinations" do
+  new_destination = params[:destination]
+  File.open('destinations.txt', 'a') do |file|
+    file.puts(new_destination)
+  end
+
+  # erb :index
+  redirect '/destinations'
+end
