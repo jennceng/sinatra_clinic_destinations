fd4bf946a15fbb77db3056734ccde537767e59ce
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-09-06 11:20:26 -0400

    remove code

--- server.rb
+++ server.rb
@@ -1,22 +1,2 @@
 require 'sinatra'
 require 'pry'
-
-get '/' do
-  redirect '/destinations'
-end
-
-get '/destinations' do
-  @destinations = File.readlines('destinations.txt')
-
-  erb :index
-end
-
-post '/destinations' do
-  destination = params[:destination]
-
-  File.open('destinations.txt', 'a') do |file|
-    file.puts(destination)
-  end
-
-  redirect '/destinations'
-end
